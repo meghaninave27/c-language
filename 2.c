4fca947e7e87a5e092f67f9f2f2a1ddfa7d73e0c
meghaninave27 <meghaninave8@gmail.com>
2024-07-18 18:08:17 +0530

    Add files via upload

---
+++ 2.c
@@ -0,0 +1,14 @@
+//Write a Program to print the given pattern.
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	printf("--------\n");
+	printf("|\t|\n");
+	printf("R\t|\n");
+	printf("N\t|\n");
+	printf("W\t|\n");
+	printf("-------");
+	return 0;
+};
+
