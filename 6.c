87c97e9e22da1142a04a277168ebf9bc73eed458
meghaninave27 <meghaninave8@gmail.com>
2024-07-18 18:09:13 +0530

    Add files via upload

---
+++ 6.c
@@ -0,0 +1,12 @@
+//Write a Program to find the area of a rectangle.
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int l,area;
+	printf("enter the l of rectangle:");
+	scanf("%d",&l);
+	area=l*l;
+	printf("area of rectangle:%d",area);
+	return 0;
+}
