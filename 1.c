46b6220aa9f73eaa168d897413f1009609aff303
meghaninave27 <meghaninave8@gmail.com>
2024-07-18 18:07:41 +0530

    Add files via upload

---
+++ 1.c
@@ -0,0 +1,13 @@
+//Write a Program to print your name, age, and school.
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	printf("megha\n");
+	printf("18\n");
+	printf("maharshi astik school");
+	return 0;
+}
+
+	
+
