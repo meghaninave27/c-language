f1885956b86c43759e416ead6264af4035c3196b
meghaninave27 <meghaninave8@gmail.com>
2024-07-25 18:23:53 +0530

    Add files via upload

---
+++ 10.c
@@ -0,0 +1,12 @@
+//Temperature Converter
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	float f,c;
+	printf("enter the value of c:");
+	scanf("%f",&c);
+    f=(1.8*c)+32;
+	printf("enter the f :%f",f);
+	return 0;
+}
